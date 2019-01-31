b32fab8dfb6f9daead4f0595f9d2e836dc42f0be
wheaton5 <whheaton@gmail.com>
2019-01-31 17:03:01 +0000

    now using fast hashset to store keys above min_count to get around requiring 2 passes on the data

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate bloom;
 extern crate flate2;
 extern crate debruijn;
 extern crate fnv;
+extern crate dna_io;
 
 use fnv::FnvHasher;
 use std::collections::{HashSet};
@@ -14,11 +15,11 @@ type FnvHashSet<V> = HashSet<V, BuildHasherDefault<FnvHasher>>;
 
 use clap::{App};
 
-use flate2::read::GzDecoder;
-use std::io;
-use std::io::prelude::*;
+//use flate2::read::GzDecoder;
+//use std::io;
+//use std::io::prelude::*;
 use std::fs::File;
-use std::io::Write;
+//use std::io::Write;
 
 use std::cmp::min;
 
@@ -26,39 +27,41 @@ use debruijn::*;
 //use debruijn::dna_string::*;
 use debruijn::kmer::*;
 
-use std::str;
-
 static mut KMER_SIZE: usize = 0;
 
-use bloom::{ASMS,CountingBloomFilter,BloomFilter};
+use bloom::CountingBloomFilter;
 
 fn main() {
     let (input_files, min, max, max_error, max_sum, output_hist, estimated_kmers) = load_params();
     let counting_bits: usize = 7;
-    let (bloom_kmer_counter, covered_kmers) = count_kmers_fastq(&input_files, counting_bits, estimated_kmers);
-    detect_het_kmers(bloom_kmer_counter, &input_files, estimated_kmers, min, max, max_error, max_sum, output_hist.to_string());
+    let (bloom_kmer_counter, covered_kmers) = count_kmers_fastq(&input_files, counting_bits, estimated_kmers, min, max);
+    detect_het_kmers(bloom_kmer_counter, covered_kmers, min, max, max_error, max_sum, output_hist.to_string());
 }
 
-fn count_kmers_fastq(kmers_in: &Vec<String>, counting_bits: usize, estimated_kmers: u32) -> (CountingBloomFilter, FnvHashSet<u64>) {
+fn count_kmers_fastq(kmers_in: &Vec<String>, 
+                    counting_bits: usize, 
+                    estimated_kmers: u32, 
+                    min_count: u32, _max_count: u32) -> 
+                        (CountingBloomFilter, FnvHashSet<u64>) {
     let mut kmer_counts: CountingBloomFilter = CountingBloomFilter::with_rate(counting_bits, 0.05, estimated_kmers);
     let mut coverage_passing_kmers: FnvHashSet<u64> = FnvHashSet::default();
+    let middle_base_mask: u64 = !(3 << (KX::K())); // make a mask that is 1's outside the two bits at the center of the kmer 
+    println!("middle base mask {:#b}",middle_base_mask);
     for kmer_file in kmers_in {
-        let file = match File::open(kmer_file) {
-            Ok(file) => file,
-            Err(error) => {
-                panic!("There was a problem opening the file: {:?}", error)
-            },
-        };
-        let gz = GzDecoder::new(file);
-        for (line_number, line) in io::BufReader::new(gz).lines().enumerate() {
-            
-            if line_number % 4 == 1 {
-                
-                //let dna: DnaString = DnaString::from_dna_string(&line.unwrap());
-                //for kmer_start in 0..(dna.len() - k_size + 1) {
-                for k in KmerX::kmers_from_ascii(&line.unwrap().as_bytes()) {
-                    //let k: KmerX = dna.get_kmer(kmer_start);
-                    kmer_counts.insert(&min(k.to_u64(), k.rc().to_u64()));
+        let reader = dna_io::DnaReader::from_path(kmer_file);
+        for record in reader {
+            for k in KmerX::kmers_from_ascii(&record.seq.as_bytes()) {
+                let to_hash = min(k.to_u64(), k.rc().to_u64());
+                match kmer_counts.insert_get_count(&to_hash) {
+                    a if a == min_count => { // add a middle base invariant hash to the coverage_passing_kmers set
+                        let middle_base_invariant = min(k.to_u64() & middle_base_mask, k.rc().to_u64() & middle_base_mask);
+                        coverage_passing_kmers.insert(middle_base_invariant);
+                    },
+                    //max_count => { // this would save memory but not allow a full histogram to be output which is useful
+                    //    let middle_base_invariant = to_hash & middle_base_mask;
+                    //    coverage_passing_kmers.remove(&middle_base_invariant);
+                    //},
+                    _ => (),
                 }
             }
         }
@@ -66,110 +69,66 @@ fn count_kmers_fastq(kmers_in: &Vec<String>, counting_bits: usize, estimated_kme
     (kmer_counts, coverage_passing_kmers)
 }
 
-const A_ALT: [u8; 3] = [1,2,3];
-const C_ALT: [u8; 3] = [0,2,3];
-const G_ALT: [u8; 3] = [0,1,3];
-const T_ALT: [u8; 3] = [0,1,2];
-
-fn get_alts(current_base: u8) -> [u8; 3] {
-    match current_base {
-        0 => A_ALT,
-        1 => C_ALT,
-        2 => G_ALT,
-        3 => T_ALT,
-        _ => panic!("not dna"),
-    }
-} 
-
-
-fn detect_het_kmers(kmer_counts: CountingBloomFilter, fastqs: &Vec<String>, 
-        estimated_kmers: u32, min_coverage: u32, max_coverage: u32, max_error: u32, max_sum: u32, 
+fn detect_het_kmers(kmer_counts: CountingBloomFilter, covered_kmers: FnvHashSet<u64>, 
+        min_count: u32, max_count: u32, max_error: u32, max_sum: u32, 
         output_hist: String) {
-    let mut visited_kmer = BloomFilter::with_rate(0.03, estimated_kmers);
-    eprintln!("counting bloom filter created, now second pass to detect het kmers");
+    
+    eprintln!("counting bloom filter created, detecting het kmers");
+    //1's except for middle bits representing middle base and upper mask 0'd out
+    let middle_base_mask: u64 = !(3 << (KX::K())) & KmerX::top_mask(KX::K()); 
+    let a_mask = middle_base_mask; // a mask you can & with a kmer to get that kmer with an A in the middle
+    let c_mask = (1 << KX::K()) | middle_base_mask; // or a C in the middle
+    let g_mask = (2 << KX::K()) | middle_base_mask; // etc
+    let t_mask = (3 << KX::K()) | middle_base_mask;
+    println!("C mask {:#b}", c_mask);
+    println!("G mask {:#b}", g_mask);
+    println!("T mask {:#b}", t_mask);
+    let masks: [u64; 4] = [a_mask, c_mask, g_mask, t_mask];
+    
     //let max_count: u32 = (2u32).pow(counting_bits as u32) - 1;
-    let mut full_hist = match File::create(output_hist) {
+    let mut full_hist: [u32; 1000] = [0;1000]; // initialize histogram vector
+    let mut alt_counts: [u32; 4] = [0,0,0,0]; let mut total_counts = 0;
+    let mut best_count = 0; let mut best_count_index = 0;
+    let mut second_best_count = 0; let mut second_best_count_index = 0;
+    for middle_base_invariant_kmer in &covered_kmers {
+        let a_hash = middle_base_invariant_kmer;  // always < than its RC so no check
+        let c_hash = middle_base_invariant_kmer | c_mask; // always < its RC so no check
+        let g_u64 = middle_base_invariant_kmer | g_mask; //could be > than its RC if palindromic. must check
+        let gmer = KmerX::from_u64(g_u64);
+        let g_hash = min(g_u64, gmer.rc().to_u64());
+        let t_u64 = middle_base_invariant_kmer | t_mask; //could be > than its RC if palindromic. must check
+        let tmer = KmerX::from_u64(t_u64);
+        let t_hash = min(t_u64, tmer.rc().to_u64());
+        alt_counts[0] = kmer_counts.estimate_count(&a_hash);
+        alt_counts[1] = kmer_counts.estimate_count(&c_hash);
+        alt_counts[2] = kmer_counts.estimate_count(&g_hash);
+        alt_counts[3] = kmer_counts.estimate_count(&t_hash);
+        for (index, count) in alt_counts.iter().enumerate() {
+            full_hist[min(full_hist.len()-1,*count as usize)] += 1;
+            total_counts += *count;
+            if *count >= best_count {
+                second_best_count = best_count;
+                second_best_count_index = best_count_index;
+                best_count = *count;
+                best_count_index = index;
+            } else if *count >= second_best_count {
+                second_best_count = *count;
+                second_best_count_index = index;
+            }
+        }
+        if best_count >= min_count && second_best_count >= min_count && best_count <= max_count && 
+            total_counts - best_count - second_best_count <= max_error && best_count + second_best_count <= max_sum {
+            let kmer = KmerX::from_u64(middle_base_invariant_kmer | masks[best_count_index]);
+            let kmer2 = KmerX::from_u64(middle_base_invariant_kmer | masks[second_best_count_index]);
+            println!("{}\t{}\t{}\t{}",kmer.to_string(), best_count.to_string(), kmer2.to_string(), second_best_count.to_string());
+        }
+        total_counts = 0; best_count = 0; best_count_index = 0; second_best_count = 0; second_best_count_index = 0; //reset counters
+        for i in 0..3 { alt_counts[i] = 0; }
+    }
+    let _full_hist = match File::create(output_hist) {
             Err(_) => panic!("couldn't open file for writing"),
             Ok(file) => file,
     };
-    let middle_base = KX::K()/2;
-    
-    
-    let mut alt_counts =  vec!(0,0,0);
-    //let mut count = 0;
-    for kmer_file in fastqs {
-        let file = match File::open(kmer_file) {
-            Ok(file) => file,
-            Err(error) => {
-                panic!("There was a problem opening file {} with error {}", kmer_file, error)
-            },
-        };
-        let gz = GzDecoder::new(file);
-        for (line_number, line) in io::BufReader::new(gz).lines().enumerate() {
-            if line_number % 4 == 1 {
-                //let dna: DnaString = DnaString::from_dna_string(&line.unwrap());
-                for mut k in KmerX::kmers_from_ascii(&line.unwrap().as_bytes()) {
-                    //for kmer_start in 0..(dna.len() - k_size + 1) {
-                    //let mut k: Kmer21 = dna.get_kmer(kmer_start);
-                    //let middle_base = k.get(k_size / 2);
-                    let krc = k.rc();
-                    let to_hash = min(k.to_u64(), krc.to_u64());
-                    if !visited_kmer.contains(&to_hash) {
-                        visited_kmer.insert(&to_hash);
-                        let count1 = kmer_counts.estimate_count(&to_hash);
-                        let mut all_alt_count = 0;
-                        if count1 > min_coverage && count1 < max_coverage {
-                            let alt_bases = get_alts(k.get(middle_base));
-                            for (alt_index, alt_base) in alt_bases.iter().enumerate() {
-                                k.set_mut(middle_base, *alt_base);
-                                let krc = k.rc();
-                                let to_hash = min(k.to_u64(), krc.to_u64());
-                                let count2 = kmer_counts.estimate_count(&to_hash);
-                                alt_counts[alt_index] = count2;
-                                if count2 > min_coverage {
-                                    visited_kmer.insert(&to_hash);
-                                }
-                                all_alt_count += count2;
-                            }
-                            for (alt_index, alt_base) in alt_bases.iter().enumerate() {
-                                let count2 = alt_counts[alt_index];
-                                //println!("{} > {} && {} < {} && {} < {} && {} <= {}",count2,min_coverage, count2, max_coverage,all_alt_count-count2, max_error, count1+count2, max_sum);
-                                if count2 > min_coverage && count2 < max_coverage && all_alt_count - count2 <= max_error && count1 + count2 <= max_sum {
-                                    //println!(" got here ");
-                                    k.set_mut(middle_base, *alt_base);
-                                    let mut to_write = k.to_string();
-                                    to_write.push_str(&"\t");
-                                    to_write.push_str(&count1.to_string());
-                                    to_write.push_str(&"\t");
-                                    k.set_mut(middle_base, *alt_base);
-                                    to_write.push_str(&k.to_string());
-                                    to_write.push_str(&"\t");
-                                    to_write.push_str(&count2.to_string());
-                                    to_write.push_str(&"\n");
-                                    //het_hist.write_all(to_write.as_bytes()).expect("Unable to write data");
-                                    //ddif let Some(ref mut x) = &het_hist {
-                                    //    x.write_all(to_write.as_bytes()).expect("Unable to write data");
-                                    //} else {
-                                    println!("{}",to_write);
-                                    //}
-                                    //match het_hist {
-                                    //    Some(mut x) => x.write_all(to_write.as_bytes()).expect("Unable to write data"),
-                                    //    None => println!("{}",to_write),
-                                    //}
-                                }
-                            }
-                        }
-                        if count1 > 5 {
-                            let mut to_write = count1.to_string();
-                            to_write.push_str(&"\n");
-                            //full_hist.write_all(to_write.as_bytes()).expect("Unable to write data");
-                            full_hist.write_all(to_write.as_bytes()).expect("Unable to write data");
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 
@@ -198,6 +157,9 @@ fn load_params() -> (Vec<String>, u32, u32, u32, u32, String, u32) {
     let max: u32 = max.to_string().parse::<u32>().unwrap();
     let kmer_size = params.value_of("kmer_size").unwrap_or("21");
     let kmer_size: usize = kmer_size.to_string().parse::<usize>().unwrap();
+    if kmer_size % 2 == 0 {
+        panic!("kmer size required to be odd");
+    }
     unsafe {
         KMER_SIZE = kmer_size;
     }
